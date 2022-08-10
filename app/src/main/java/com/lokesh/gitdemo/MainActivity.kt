fb58e231981c09d71be2df8e7cd828eeda31dbac
Lokesh <lokeshkumarp@helixtechsolutions.com>
2022-08-10 15:00:34 +0530

    Feture 2 changes

--- app/src/main/java/com/lokesh/gitdemo/MainActivity.kt
+++ app/src/main/java/com/lokesh/gitdemo/MainActivity.kt
@@ -8,5 +8,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //Added a commit to see staging area
+
+        //Feature 2 changes
     }
 }
